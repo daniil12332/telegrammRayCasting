bae5442fbe81bfb28c7c4d53fec11b4ab4d6af78
daniil12332 <87269045+daniil12332@users.noreply.github.com>
2024-07-13 08:55:32 +0300

    textures

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 import math
 import time
 from math import cos, sin
+from PIL import Image, ImageDraw
 
 import pygame
 
 from settings import *
 
-fps = 5
+fps = 30
 time_delta = 1./fps
 
+img = Image.open(TEXTURE)
+img = img.convert("RGB")
+d = img.getdata()
+
+imageRGB = []
+for i in range(img.size[1]):
+    imageRGB.append([])
+
+index = 0
+for item in d:
+    imageRGB[index].append(item)
+    if index == img.size[1]-1:
+        index = 0-1
+    index += 1
+
 pygame.display.init()
-surface = pygame.display.set_mode((600, 600))
+surface = pygame.display.set_mode((WIDTH, HEIGHT))
 surface.fill(WHITE)
 
-rectSizeX = (pygame.display.get_window_size()[0]) // gameWidth
-rectSizeY = (pygame.display.get_window_size()[1]) // gameHeight
+rectSizeX = WIDTH // gameWidth
+rectSizeY = HEIGHT // gameHeight
 playerX = rectSizeX*STARTPLAYERX+rectSizeX//2-rectSizeX
 playerY = rectSizeY*STARTPLAYERY+rectSizeY//2-rectSizeY
 playerAngle = 0
@@ -39,15 +55,15 @@ while run:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        playerX = int(playerX+rectSizeX*cos(math.radians(playerAngle)))
-        playerY = int(playerY+rectSizeY*sin(math.radians(playerAngle)))
+        playerX = int(playerX+PLAYERSPEED*cos(math.radians(playerAngle)))
+        playerY = int(playerY+PLAYERSPEED*sin(math.radians(playerAngle)))
     if keys[pygame.K_s]:
         playerX = int(playerX-PLAYERSPEED*cos(math.radians(playerAngle)))
         playerY = int(playerY-PLAYERSPEED*sin(math.radians(playerAngle)))
     if keys[pygame.K_a]:
-        playerAngle -= 90
+        playerAngle -= PLAYERANGLESPEED
     if keys[pygame.K_d]:
-        playerAngle += 90
+        playerAngle += PLAYERANGLESPEED
     
     #for i in walls:
     #    pygame.draw.rect(surface, GREEN, i)
@@ -63,8 +79,19 @@ while run:
                     break
             if cx > wall[0] and cx < wall[0] + wall[2] and cy > wall[1] and cy < wall[1] + wall[3]:
                 break
-        col = CWORLD/(c*cos(lineAngle-playerAngle))
-        pygame.draw.line(surface, BLUE, (i*GRAPHUNDETAL, SCREENWIDTH//2-col), (i*GRAPHUNDETAL, SCREENWIDTH//2+col), GRAPHUNDETAL)
+        col = int(CWORLD/(c*cos(lineAngle-playerAngle)))
+        #pygame.draw.line(surface, BLUE, (i*GRAPHUNDETAL, SCREENWIDTH//2-col), (i*GRAPHUNDETAL, SCREENWIDTH//2+col), GRAPHUNDETAL)
+        hitX = (cx-wall[0])/rectSizeX
+        hitY = (cy-wall[1])/rectSizeY
+        if hitX == 0.01 or hitX == 0.99:
+            hit = hitY
+        else:
+            hit = hitX
+        columnRGB = imageRGB[int(hit*img.size[0])]
+        for j in range(col*2):
+            pix_y = int(j+SCREENWIDTH//2-col)
+            #print(pix_y)
+            pygame.draw.line(surface, columnRGB[int(img.size[0]*(j/(col*2)))], (i*GRAPHUNDETAL, pix_y), (i*GRAPHUNDETAL, pix_y), GRAPHUNDETAL)
         #pygame.draw.line(surface, RED, (playerX, playerY), (cx, cy))
     playerAngle = math.degrees(playerAngle)
         
