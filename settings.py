64a446fc619eadb74ccdaea2558862e8a6459258
daniil12332 <87269045+daniil12332@users.noreply.github.com>
2024-07-13 08:56:01 +0300

    textures

--- settings.py
+++ settings.py
@@ -6,9 +6,15 @@ PLAYERSPEED = 5
 PLAYERANGLESPEED = 5
 
 FOV = 1.05
-CWORLD = 10000
+CWORLD = 15000
 GRAPHUNDETAL = 10
+TEXTURE = "telegrammRayCasting/texture.jpg"
+IMAGE="telegrammRayCasting/game.jpg"
+MAP = "telegrammRayCasting/map.jpg"
+ANIMATION = "telegrammRayCasting/animation.gif"
 
+WIDTH = 600
+HEIGHT = 600
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -17,7 +23,7 @@ BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-TOKEN = "token"
+TOKEN = "6322116336:AAHbrwy4QboHcgywG6cHcuY7GSyeZTk1-fk"
 
 gameWidth = 6
 gameHeight = 7
